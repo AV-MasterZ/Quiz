7b02db7b7eb0c1802488e56c2cd71c452b24636e
Alexander Zhadaev <a.v.zhadaev@gmail.com>
2020-08-16 13:06:18 +0400

    added @Repository

--- User/src/main/java/org/zhadaev/user/repository/IRoleRepository.java
+++ User/src/main/java/org/zhadaev/user/repository/IRoleRepository.java
@@ -1,8 +1,10 @@
 package org.zhadaev.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.zhadaev.user.model.Role;
 
+@Repository
 public interface IRoleRepository extends JpaRepository<Role, Long> {
     Role findByName(final String name);
 }
--- User/src/main/java/org/zhadaev/user/repository/IUserRepository.java
+++ User/src/main/java/org/zhadaev/user/repository/IUserRepository.java
@@ -1,8 +1,10 @@
 package org.zhadaev.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.zhadaev.user.model.User;
 
+@Repository
 public interface IUserRepository extends JpaRepository<User, Long> {
     User findByUsername(final String username);
 }
