4602fef17932a575fef1ada1f25317cbb55e1d11
Alexander Zhadaev <a.v.zhadaev@gmail.com>
2020-08-20 23:55:03 +0400

    added UserAnswer entity

---
+++ Questionnaires/src/main/java/org/zhadaev/questionnaires/model/UserAnswer.java
@@ -0,0 +1,47 @@
+package org.zhadaev.questionnaires.model;
+
+import org.zhadaev.constructor.model.Answer;
+import org.zhadaev.user.model.User;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users_answers")
+public class UserAnswer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "answer_id", nullable = false)
+    private Answer answer;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(final User user) {
+        this.user = user;
+    }
+
+    public Answer getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(final Answer answer) {
+        this.answer = answer;
+    }
+}
