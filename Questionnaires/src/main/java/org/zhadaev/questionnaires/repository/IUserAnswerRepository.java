ef8d78e5fd3937407946a12b6241920be6c9e9a3
Alexander Zhadaev <a.v.zhadaev@gmail.com>
2020-08-21 01:06:27 +0400

    added findByQuestionnaireId()

--- Questionnaires/src/main/java/org/zhadaev/questionnaires/repository/IUserAnswerRepository.java
+++ Questionnaires/src/main/java/org/zhadaev/questionnaires/repository/IUserAnswerRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.zhadaev.questionnaires.model.UserAnswer;
 
+import java.util.Set;
+
 @Repository
 public interface IUserAnswerRepository extends JpaRepository<UserAnswer, Long> {
+    Set<UserAnswer> findByQuestionnaireId(final Long id);
 }
