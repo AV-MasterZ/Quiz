75a118734851a45ec0e6a7e5a91afea7d4f52425
Alexander Zhadaev <a.v.zhadaev@gmail.com>
2020-08-21 14:28:31 +0400

    added findByAnswerId()

--- Questionnaires/src/main/java/org/zhadaev/questionnaires/repository/IUserAnswerRepository.java
+++ Questionnaires/src/main/java/org/zhadaev/questionnaires/repository/IUserAnswerRepository.java
@@ -10,4 +10,5 @@ import java.util.Set;
 public interface IUserAnswerRepository extends JpaRepository<UserAnswer, Long> {
     Set<UserAnswer> findByUserId(final Long id);
     UserAnswer findByUserIdAndAnswerId(final Long userId, final Long answerId);
+    UserAnswer findByAnswerId(final Long answerId);
 }
